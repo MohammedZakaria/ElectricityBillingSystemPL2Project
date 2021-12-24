358b6a748b4bc977e1c5eb66dba0663d4515439f
Mohamed Zakarya <mohammedzakaria032@gmail.com>
2021-12-25 01:12:09 +0200

    Nothing

--- src/main/java/org/pl/electricitybillingsystempl2project/HelloController.java
+++ src/main/java/org/pl/electricitybillingsystempl2project/HelloController.java
@@ -1,14 +1,11 @@
 package org.pl.electricitybillingsystempl2project;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
     }
 }
