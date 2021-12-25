644bbf5edf3b9c29c5baea49467464c9e12f4f9f
Mohamed Zakarya <mohammedzakaria032@gmail.com>
2021-12-25 21:35:12 +0200

    Categorizing and Grouping Methods into Classes

---
+++ src/main/java/org/pl/electricitybillingsystempl2project/Controllers/OperatorCustomerDetails.java
@@ -0,0 +1,35 @@
+package org.pl.electricitybillingsystempl2project.Controllers;
+
+import javafx.fxml.FXML;
+
+public class OperatorCustomerDetails {
+    @FXML
+    protected void operatorCustomerDetailsPrintBill() {
+
+    }
+
+    @FXML
+    protected void operatorCustomerDetailsCollectPayment() {
+
+    }
+
+    @FXML
+    protected void operatorCustomerDetailsValidateRealConsumption() {
+
+    }
+
+    @FXML
+    protected void operatorCustomerDetailsCancelSuscribtion() {
+
+    }
+
+    @FXML
+    protected void operatorCustomerDetailsDownloadContract() {
+
+    }
+
+    @FXML
+    protected void operatorCustomerDetailsCancel() {
+
+    }
+}
---
+++ src/main/java/org/pl/electricitybillingsystempl2project/Controllers/UserEdit.java
@@ -0,0 +1,21 @@
+package org.pl.electricitybillingsystempl2project.Controllers;
+
+import javafx.fxml.FXML;
+
+public class UserEdit {
+    @FXML
+    protected void Exit() {
+
+    }
+
+    @FXML
+    protected void userEditApplyChanges() {
+
+    }
+
+    @FXML
+    protected void userEditCancel() {
+
+    }
+
+}
