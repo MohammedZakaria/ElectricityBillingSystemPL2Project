2131175c65067780d409f8cedfd0968aedd281a8
Ahmedashraftaha <92655061+Ahmedashraftaha@users.noreply.github.com>
2021-12-28 21:55:47 +0200

    add new operator

--- src/main/java/org/pl/electricitybillingsystempl2project/Controllers/AddNewOperator.java
+++ src/main/java/org/pl/electricitybillingsystempl2project/Controllers/AddNewOperator.java
@@ -1,11 +1,37 @@
 package org.pl.electricitybillingsystempl2project.Controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import org.pl.electricitybillingsystempl2project.entities.Operator;
+import org.pl.electricitybillingsystempl2project.entitymanager.EntityManager;
+import org.pl.electricitybillingsystempl2project.entitymanager.EntityManagerFactory;
 
 public class AddNewOperator {
     @FXML
-    protected void registerOperator() {
+    private TextField nameTxt;
+    @FXML
+    private TextField phoneTxt;
+    @FXML
+    private TextField passwordTxt;
+    @FXML
+    private TextField confirm_passwordTxt;
 
+    protected void registerOperator() {
+String name =nameTxt.getText();
+String phone=phoneTxt.getText();
+String password=passwordTxt.getText();
+String confirmPassword=confirm_passwordTxt.getText();
+        Operator op =new Operator();
+        op.setName(name);
+        op.setPhone(phone);
+        op.setPassword(password);
+        if(op.getPassword()==confirmPassword){
+            EntityManagerFactory.getEntityManager(Operator.class).save(op)
+                    .onFailure(t->   new Alert(Alert.AlertType.ERROR, "Not save Operator").show());
+        }else
+            new Alert(Alert.AlertType.ERROR, "Confirm Password should be like password ").show();
+        
     }
 
     @FXML
