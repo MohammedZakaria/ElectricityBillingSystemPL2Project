e0b5a805b6022e557c1c8f9c42f3c2489c458631
ASUS <passant.ibrahim228@gmail.com>
2021-12-28 23:36:41 +0200

    RegisterCustomer function implementation

--- src/main/java/org/pl/electricitybillingsystempl2project/Controllers/AddNewCustomer.java
+++ src/main/java/org/pl/electricitybillingsystempl2project/Controllers/AddNewCustomer.java
@@ -1,11 +1,27 @@
 package org.pl.electricitybillingsystempl2project.Controllers;
 
+
+import java.awt.Image;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javax.swing.ImageIcon;
+import javax.swing.JFileChooser;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import javax.swing.JOptionPane;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import org.pl.electricitybillingsystempl2project.entities.Admin;
 import org.pl.electricitybillingsystempl2project.entities.Customer;
 import org.pl.electricitybillingsystempl2project.entitymanager.EntityManager;
 import org.pl.electricitybillingsystempl2project.entitymanager.EntityManagerFactory;
+
+import java.io.File;
 
 public class AddNewCustomer {
     @FXML
@@ -34,7 +50,6 @@ public class AddNewCustomer {
 
     }
 
-        }
 
     @FXML
     protected void uploadContract() {
