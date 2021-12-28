235d73c87902567422e22d54d99b25b94412ddd7
ASUS <passant.ibrahim228@gmail.com>
2021-12-28 22:05:52 +0200

    RegisterCustomer function implementation

--- src/main/java/org/pl/electricitybillingsystempl2project/Controllers/AddNewCustomer.java
+++ src/main/java/org/pl/electricitybillingsystempl2project/Controllers/AddNewCustomer.java
@@ -1,6 +1,11 @@
 package org.pl.electricitybillingsystempl2project.Controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import org.pl.electricitybillingsystempl2project.entities.Admin;
+import org.pl.electricitybillingsystempl2project.entities.Customer;
+import org.pl.electricitybillingsystempl2project.entitymanager.EntityManager;
+import org.pl.electricitybillingsystempl2project.entitymanager.EntityManagerFactory;
 
 public class AddNewCustomer {
     @FXML
@@ -10,6 +15,19 @@ public class AddNewCustomer {
 
     @FXML
     protected void registerCustomer() {
+       Customer customer = new Customer();
+        String name = addNewCustName.getText();
+        String  email = addNewCustEmail.getText();
+        String password = addNewCustPassword.getText();
+        String Confirm_Password = addNewCustConfirmPassword.getText();
+        String Phone = addNewCustPhone.getText();
+        customer.setName(name);
+        customer.setEmail(email);
+        customer.setPassword(password);
+        customer.setPhone(Phone);
+
+        EntityManagerFactory.getEntityManager(Customer.class).save(customer)
+                .onFailure(t ->new Alert(Alert.AlertType.ERROR, "Something went Wrong").show());
 
     }
 
