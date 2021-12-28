17f9119fad7b4e453ec19256a14838ddb5a3ba7b
ASUS <passant.ibrahim228@gmail.com>
2021-12-29 00:42:00 +0200

    RegisterCustomer function implementation

--- src/main/java/org/pl/electricitybillingsystempl2project/Controllers/AddNewCustomer.java
+++ src/main/java/org/pl/electricitybillingsystempl2project/Controllers/AddNewCustomer.java
@@ -1,29 +1,24 @@
 package org.pl.electricitybillingsystempl2project.Controllers;
 
-
-import java.awt.Image;
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.event.ActionEvent;
-import javafx.scene.control.Label;
-import javax.swing.ImageIcon;
-import javax.swing.JFileChooser;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import javax.swing.JOptionPane;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
 import org.pl.electricitybillingsystempl2project.entities.Admin;
 import org.pl.electricitybillingsystempl2project.entities.Customer;
 import org.pl.electricitybillingsystempl2project.entitymanager.EntityManager;
 import org.pl.electricitybillingsystempl2project.entitymanager.EntityManagerFactory;
 
-import java.io.File;
-
 public class AddNewCustomer {
+    @FXML
+    private TextField addNewCustName;
+    @FXML
+    private TextField addNewCustEmail;
+    @FXML
+    private TextField addNewCustPassword;
+    @FXML
+    private TextField addNewCustConfirmPassword;
+    @FXML
+    private TextField addNewCustPhone;
     @FXML
     protected void cancelCustomer() {
         ControllersUtils.closePageWithNode(addNewCustName);
@@ -32,7 +27,7 @@ public class AddNewCustomer {
 
     @FXML
     protected void registerCustomer() {
-       Customer customer = new Customer();
+        Customer customer = new Customer();
         String name = addNewCustName.getText();
         String  email = addNewCustEmail.getText();
         String password = addNewCustPassword.getText();
@@ -51,10 +46,9 @@ public class AddNewCustomer {
     }
 
 
+
     @FXML
     protected void uploadContract() {
-        JFileChooser Contract = new JFileChooser();
-        File f = Contract.getSelectedFile();
 
     }
 }
